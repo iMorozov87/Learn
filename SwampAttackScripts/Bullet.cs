43885843773953f634ebfbab196634e4c5f93ca8
iMorozov87 <59395265+iMorozov87@users.noreply.github.com>
2020-05-30 00:50:20 +0300

    Add files via upload

---
+++ SwampAttackScripts/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] float _timeToDestroy = 5;
+    [SerializeField] private int _demage;
+    [SerializeField] private float _speed;
+
+    private void Start()
+    {
+        Destroy(gameObject, _timeToDestroy);
+    }
+    private void Update()
+    {
+        transform.Translate(Vector2.left * _speed * Time.deltaTime, Space.Self);       
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.TryGetComponent<Enemy>(out Enemy enemy))
+        {
+            enemy.TakeDemage(_demage);
+            Destroy(gameObject);        
+        }
+    }
+}
---
+++ SwampAttackScripts/Shotgun.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shotgun : Weapon
+{
+    [SerializeField] private int _numbersOfBullets = 10;
+
+    public override void Shoot(Transform shootPoint)
+    {
+        int minAngle = -30;
+        int maxAngle = 30;
+        float steepChangedAngle = (float)(maxAngle - minAngle) / _numbersOfBullets;
+        float currentAngle = maxAngle;
+
+        for (int i = 0; i < _numbersOfBullets; i++)
+        {
+            Bullet newBullet = Instantiate(Bullet, shootPoint.position, Quaternion.Euler(0, 0, currentAngle));
+            currentAngle -= steepChangedAngle;
+        }
+    }
+}
